d1371dbda62c1c702b3c7b83c621889dfeb5aab9
João Victor Rondon Vincensi <jvrvincensi@gmail.com>
2022-07-04 16:30:49 -0400

    Version 1.01

---
+++ Projeto_05/23-rest copy/scripts.js
@@ -0,0 +1,15 @@
+let num0 = 0;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+function imprimirNumeros(args){
+    for(let i = 0; i < args.length; i++){
+        console.log(args[i]);
+    }
+}
+imprimirNumeros(num0);
+imprimirNumeros(num1);
+imprimirNumeros(num2);
+imprimirNumeros(num3);
+imprimirNumeros(2,52,7,5,1,4,8,8,69,41,);
